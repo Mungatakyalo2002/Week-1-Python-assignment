7bd2eaae6804d12bf799434a701a91dba46d6e88
Mungatakyalo2002 <rmungata@gmail.com>
2025-08-27 19:50:46 +0300

    Second Commit message. Had to edit my code

--- calculator.py
+++ calculator.py
@@ -8,18 +8,16 @@ num2 = float(input("Enter the second number: "))
 operation = input("Enter operation (+, -, *, /): ")
 
 # Perform the operation and display the result
-if operation == "+":
-    result = num1 + num2
-    print(f"{num1} + {num2} = {result}")
-elif operation == "-":
-    result = num1 - num2
-    print(f"{num1} - {num2} = {result}")
-elif operation == "*":
-    result = num1 * num2
-    print(f"{num1} * {num2} = {result}")
-elif operation == "/":
-    if num2 != 0:  
-        result = num1 / num2
-        print(f"{num1} / {num2} = {result}")
-    else:
-        print("Math Error: Cannot divide by zero!")
+
+result = num1 + num2
+print(f"{num1} + {num2} = {result}")
+
+result = num1 - num2
+print(f"{num1} - {num2} = {result}")
+
+result = num1 * num2
+print(f"{num1} * {num2} = {result}")
+
+result = num1 / num2
+print(f"{num1} / {num2} = {result}")
+    
